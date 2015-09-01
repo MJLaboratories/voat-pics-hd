bdf9ac98a6a452ce2d0bf7c13d8205287cd320cd
kannom <matcannon1@gmail.com>
2015-09-01 21:54:08 +0100

    Adding reference to swiper object for slide navigation

--- voatPicsHd/www/js/controllers.js
+++ voatPicsHd/www/js/controllers.js
@@ -1,38 +1,48 @@
 angular.module('starter.controllers', [])
 
   .controller('FrontPageCtrl', function ($scope, VoatPostalService, $timeout) {
-    $scope.doRefresh = function(){
-      $timeout(function(){
-        VoatPostalService.all().then(function(voatPosts) {
-          $scope.posts = voatPosts;
+    $scope.doRefresh = function () {
+      $timeout(function () {
+        VoatPostalService.all().then(function (voatPosts) {
+          $scope.voatPosts = voatPosts;
           $scope.$broadcast('scroll.refreshComplete');
         });
-      },100);
+      }, 100);
     };
 
-    $scope.posts = [];
+    $scope.doRefresh();
+
+    $scope.voatPosts = [];
 
     $scope.remove = function (post) {
       VoatPostalService.remove(post);
     };
   })
 
-  .controller('GalleryCtrl', function ($scope, $stateParams, VoatPostalService) {
-    // do we want this to load data?
-    VoatPostalService.all().then(function(voatPosts) {
-      $scope.images = voatPosts;
+  .controller('GalleryCtrl', function ($rootScope, $scope, $stateParams, VoatPostalService) {
+
+    //We can use the swiper object to navigate to certain slides. Just need to get routing setup to get the id from the list view.
+    $scope.swiper = {};
+
+    $scope.onReadySwiper = function (swiper) {
+
+      $scope.swiper = swiper;
+    };
+
+    VoatPostalService.all().then(function (voatPosts) {
+      $scope.voatPosts = voatPosts;
     });
 
-    if (!$scope.images) {
+    if (!$scope.voatPosts) {
       return;
     }
 
     // please replace with nice way to select from an array - I can't remember how'
-      for (var i = 0; i < $scope.images.length; i++) {
-        if ($scope.images[i].id === parseInt($stateParams.id)) {
-          $scope.activeSlide = $scope.images[i];
-        }
+    for (var i = 0; i < $scope.images.length; i++) {
+      if ($scope.images[i].id === parseInt($stateParams.id)) {
+        $scope.activeSlide = $scope.images[i];
       }
+    }
   });
 
 
