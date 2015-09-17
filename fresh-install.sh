4818d7091b6ad43888938326e02663c50fae97a5
knowncolor <github@knowncolor,com>
2015-09-17 14:32:18 +0100

    Updated gallery view:  - changed to use ion-slider controls  - added title at bottom of page  - tap image to switch between scale-to-fit and maximised view  - added ability to scroll maximised image
    New script to install and update necessary components
    
    Updated .gitignore for dynamically generated files

---
+++ fresh-install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd voatPicsHd
+sudo npm install -g cordova ionic gulp bower
+npm install
+bower install
+
--- voatPicsHd/www/js/controller/gallery-controller.js
+++ voatPicsHd/www/js/controller/gallery-controller.js
@@ -1,5 +1,5 @@
 var module = angular.module('app.controllers');
-module.controller('GalleryCtrl', function ($scope, $stateParams, $cacheFactory) {
+module.controller('GalleryCtrl', function ($scope, $stateParams, $cacheFactory, $ionicSlideBoxDelegate, $ionicScrollDelegate) {
 
   function findWithAttr(array, attr, value) {
     for (var i = 0; i < array.length; i += 1) {
@@ -9,10 +9,43 @@ module.controller('GalleryCtrl', function ($scope, $stateParams, $cacheFactory) 
     }
   }
 
+
+  $scope.fitImageToScreen = true;
+  var slideEnabled = true;
+
+
+  function toggleFitImageToScreen() {
+    $scope.fitImageToScreen = !$scope.fitImageToScreen;
+    $scope.$broadcast('scroll.resize');
+  };
+
+  function toggleSwiperEnabled () {
+    slideEnabled = !slideEnabled;
+    $ionicSlideBoxDelegate.enableSlide(slideEnabled);
+  };
+
+  function resetImagePosition () {
+    var dontAnimate = false;
+    $ionicScrollDelegate.scrollTo(0, 0, dontAnimate);
+  }
+
+  $scope.slideHasChanged = function ($index) {
+    console.log("active slide changed: " + $index);
+  };
+  
+  $scope.imageTapped = function () {
+    console.log("image tapped");
+    toggleFitImageToScreen();
+    toggleSwiperEnabled();
+    resetImagePosition();
+
+    // possible to use zoom animations and scroll to tapped location
+    // see http://forum.ionicframework.com/t/how-to-get-pagex-and-pagey-for-on-tap-event/6817/2
+    // http://ionicframework.com/docs/api/service/$ionicScrollDelegate/
+  };
+
   var httpCache = $cacheFactory.get('voatPosts');
   $scope.voatPosts = httpCache.get('voatPosts');
   $scope.activeSlideId = parseInt($stateParams.id);
-
-  $scope.initialSlideIndex = findWithAttr($scope.voatPosts, 'id', $scope.activeSlideId);
-
+  $scope.activeSlideIndex = findWithAttr($scope.voatPosts, 'id', $scope.activeSlideId);
 });
